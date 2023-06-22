6fab8d7feff46095417395e97f178600a5356223
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-06-22 11:31:22 +0200

    Update env.ts

--- dtd-catalog-exporter/src/configs/env.ts
+++ dtd-catalog-exporter/src/configs/env.ts
@@ -20,4 +20,10 @@ export const envSchema = z.object({
 export type Env = z.infer<typeof envSchema>;
 
 dotenv.config();
-export const env = envSchema.parse(process.env);
+
+// Skip env runtime check if we are in test environment
+if (process.env.NODE_ENV !== "test") {
+  envSchema.parse(process.env);
+}
+
+export const env = process.env as Env;
