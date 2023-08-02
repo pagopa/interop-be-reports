72f64a16d14a08b2d09cb417091342d73396494b
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-08-02 11:50:07 +0200

    Extended jsdoc comment to `toCsvDataRow` functino

--- registered-comuni-pn/src/utils/helpers.utils.ts
+++ registered-comuni-pn/src/utils/helpers.utils.ts
@@ -34,6 +34,14 @@ export class SafeMap<K, V> extends Map<K, V> {
 
 /**
  * Transforms a purpose to a CSV output row.
+ * The row contains the following fields:
+ * - nome_comune: The name of the tenant related to the purpose, retrived from the tenants map
+ * - stato_finalita_migliore: The state of the purpose. It follows the following logic:
+ *    1. If the purpose has an active version, the state is "Attivo";
+ *    2. If the purpose has no active version, but has a suspended version, the state is "Sospeso";
+ *    3. If the purpose has no active or suspended version, but has a waiting for approval version, the state is "In attesa di attivazione";
+ *    4. If the purpose has no active, suspended or waiting for approval version, an error is thrown. This should never happen since the query that retrieves the purposes filters out purposes that do not have an active, suspended or waiting for approval version.
+ * - data_attivazione: The first activation date of the purpose. It is retrieved from the firstActivationAt field of the active, suspended or waiting for approval version.
  */
 export function toCsvDataRow(tenantNamesMap: SafeMap<string, string>, purpose: Purpose) {
   function getPurposeVersionWithState(state: PurposeState) {
