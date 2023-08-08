25abd684efdb095c25bbb2a7f5cb3381a623f221
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-08-08 19:18:46 +0200

    Updated tests

--- jobs/commons/src/utils/__tests__/helpers.utils.test.ts
+++ jobs/commons/src/utils/__tests__/helpers.utils.test.ts
@@ -1,4 +1,9 @@
-import { SafeMap, toCSV } from '../helpers.utils.js'
+import {
+  SafeMap,
+  getSafeMapFromIdentifiableRecords,
+  toCSV,
+  withExecutionTime,
+} from '../helpers.utils.js'
 
 describe('toCSV', () => {
   it('should return a CSV string from an array of objects', () => {
@@ -24,5 +29,37 @@ describe('SafeMap', () => {
 
     expect(() => safeMap.get('key')).not.toThrow()
     expect(() => safeMap.get('wrongKey')).toThrow()
+  })
+})
+
+describe('getSafeMapFromIdentifiableRecords', () => {
+  it('should return a SafeMap from an array of identifiable records', () => {
+    const identifiableRecords = [
+      { id: 'id1', name: 'name1' },
+      { id: 'id2', name: 'name2' },
+    ]
+
+    const expected = new SafeMap<string, { id: string; name: string }>([
+      ['id1', { id: 'id1', name: 'name1' }],
+      ['id2', { id: 'id2', name: 'name2' }],
+    ])
+
+    expect(getSafeMapFromIdentifiableRecords(identifiableRecords)).toEqual(expected)
+  })
+})
+
+describe('withExecutionTime', () => {
+  const mockFn = vitest.fn()
+  beforeAll(() => {
+    vitest.spyOn(console, 'log').mockImplementation(mockFn)
+  })
+
+  afterAll(() => {
+    vitest.restoreAllMocks()
+  })
+
+  it('should call the function and log the execution time', async () => {
+    await withExecutionTime(mockFn)
+    expect(mockFn).toHaveBeenCalledWith(expect.stringContaining('Execution time: '))
   })
 })
