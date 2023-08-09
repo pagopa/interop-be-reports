2c4cbf266e0f26dd9fee774cdd00a46831c31813
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-08-09 14:57:10 +0200

    Renamed common models files

--- jobs/commons/src/models/index.ts
+++ jobs/commons/src/models/index.ts
@@ -1,4 +1,4 @@
-export * from './attributes.models.js'
+export * from './attribute.model.js'
 export * from './e-service.models.js'
-export * from './tenants.models.js'
-export * from './purposes.models.js'
+export * from './tenant.model.js'
+export * from './purpose.model.js'
