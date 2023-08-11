eeb3f811bc3a1a6a71dcd3f48df3dd63d3a78b0e
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-08-11 11:17:41 +0200

    PIN-3695: Handle message with non-required product

--- jobs/selfcare-events-consumer/src/processor.ts
+++ jobs/selfcare-events-consumer/src/processor.ts
@@ -2,21 +2,27 @@ import { KafkaMessage } from "kafkajs";
 import { EventPayload } from "./model/InstitutionEvent.js";
 
 
-export async function processMessage(message: KafkaMessage): Promise<void> {
-  if (message.value) {
-    const stringPayload = message.value.toString()
-    const jsonPayload = JSON.parse(stringPayload);
-    const parsed = EventPayload.safeParse(jsonPayload);
-    if (parsed.success) {
-      // TODO Filter by product
-      console.log(`Message with offset ${message.offset} correctly received`);
-    } else {
-      console.log(`Error consuming message with offset ${message.offset}. Reason: ${parsed.error}. Message: ${stringPayload}`)
-      throw parsed.error;
-    }
-  } else {
+export const processMessage = (productName: string) => async (message: KafkaMessage): Promise<void> => {
+  if (!message.value)
     // TODO Should this throw an error or log warning and ignore?
-    throw Error(`Empty content for message with offset ${message.offset}`);
+    // throw Error(`Empty content for message with offset ${message.offset}`);
+    return
+
+  const stringPayload = message.value.toString()
+  const jsonPayload = JSON.parse(stringPayload);
+
+  // Process only messages of our product
+  // Note: Filtered before parsing to avoid errors on an unexpected messages that we are not interested in 
+  if (jsonPayload.product !== productName)
+  // TODO Log debug?
+    return
+
+  const parsed = EventPayload.safeParse(jsonPayload);
+  if (parsed.success) {
+    console.log(`Message with offset ${message.offset} correctly received`);
+  } else {
+    console.log(`Error consuming message with offset ${message.offset}. Reason: ${parsed.error}. Message: ${stringPayload}`)
+    throw parsed.error;
   }
 }
 
