0eaea9f9ba6c4011c14ddfc52540c220dffb9981
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-08-30 12:35:38 +0200

    Added `code` property to attribute schema

--- jobs/commons/src/models/attribute.model.ts
+++ jobs/commons/src/models/attribute.model.ts
@@ -4,6 +4,7 @@ export const attributeSchema = z.object({
   id: z.string(),
   name: z.string(),
   description: z.string(),
+  code: z.string().optional(),
 })
 
 export type Attribute = z.infer<typeof attributeSchema>
