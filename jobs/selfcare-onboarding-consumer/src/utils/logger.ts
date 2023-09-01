12c68f04ee589cf3b6a363d19bd9538050bf4dd3
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-09-01 14:10:11 +0200

    PIN-3813: Logger default value

--- jobs/selfcare-onboarding-consumer/src/utils/logger.ts
+++ jobs/selfcare-onboarding-consumer/src/utils/logger.ts
@@ -2,8 +2,8 @@
 
 const timestamp = () => new Date().toISOString()
 
-export const info = (correlationId: string, message: string) : void => console.log(`${timestamp()} INFO [CID=${correlationId}] ${message}`)
+export const info = (correlationId: string, message: string): void => console.log(`${timestamp()} INFO [CID=${correlationId}] ${message}`)
 
-export const warn = (correlationId: string, message: string) : void => console.log(`${timestamp()} WARN [CID=${correlationId}] ${message}`)
+export const warn = (correlationId: string, message: string): void => console.log(`${timestamp()} WARN [CID=${correlationId}] ${message}`)
 
-export const error = (correlationId: string, message: string, reason: any | undefined) : void => console.error(`${timestamp()} ERROR [CID=${correlationId}] ${message}`, reason)
+export const error = (correlationId: string, message: string, reason: any | undefined = undefined): void => console.error(`${timestamp()} ERROR [CID=${correlationId}] ${message}`, reason)
