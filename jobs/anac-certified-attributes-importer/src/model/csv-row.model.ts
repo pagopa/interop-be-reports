9050bc3d8cdde9620c86f527bf0a7c49b98f9c51
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-09-04 18:21:02 +0200

    PIN-3813: Remove unused comments

--- jobs/anac-certified-attributes-importer/src/model/csv-row.model.ts
+++ jobs/anac-certified-attributes-importer/src/model/csv-row.model.ts
@@ -1,16 +1,5 @@
 import { z } from "zod";
 
-// export const CsvRow = z.object({
-//   cf_gestore: z.string().min(1),
-//   denominazione: z.string().min(1),
-//   domicilio_digitale: z.string().min(1),
-//   codice_IPA: z.string().optional().transform((value) => value === '' ? undefined : value),
-//   anac_incaricato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-//   anac_abilitato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-// });
-
-// export type CsvRow = z.infer<typeof CsvRow>
-
 export const PaRow = z.object({
   cf_gestore: z.string().min(1),
   denominazione: z.string().min(1),
@@ -31,13 +20,6 @@ export const NonPaRow = z.object({
   anac_in_convalida: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
 });
 export type NonPaRow = z.infer<typeof NonPaRow>
-
-// export const CsvRow = z.discriminatedUnion("codice_IPA", [
-//   z.object({ codice_IPA: z.string().length(0).optional() }).transform(v => v.codice_IPA = undefined).merge(NonPaRow), 
-
-//   // z.object({ codice_IPA: z.string().optional().length(0) }).merge(NonPaRow).transform(v => v.codice_IPA = undefined), //.omit({codice_IPA : true}) //
-//   z.object({ codice_IPA: z.string().min(1) }).merge(PaRow),
-// ]);
 
 export const CsvRow = PaRow.or(NonPaRow)
 
