1188fd98303673629d6c558a7f96b6acd2dbd4ba
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-09-04 14:00:12 +0200

    PIN-3813: Test

--- jobs/anac-certified-attributes-importer/src/service/__tests__/helpers.ts
+++ jobs/anac-certified-attributes-importer/src/service/__tests__/helpers.ts
@@ -1,4 +1,5 @@
-import { InteropContext, PersistentAttribute, PersistentTenant } from "../../model/index.js"
+import { ANAC_ABILITATO_CODE, ANAC_INCARICATO_CODE, ANAC_IN_CONVALIDA_CODE } from "../../config/index.js"
+import { InteropContext, PersistentAttribute, PersistentTenant, PersistentTenantAttribute } from "../../model/index.js"
 
 export const sftpConfigTest = {
   host: "host",
@@ -15,6 +16,10 @@ const csvFileContent =
 0011223344,"E-Procurement 2 con , virgola nel nome",eprocurement1@pec.it,,TRUE,TRUE,FALSE`
 
 
+export const ATTRIBUTE_ANAC_INCARICATO_ID = 'b1d64ee0-fda9-48e2-84f8-1b62f1292b47'
+export const ATTRIBUTE_ANAC_ABILITATO_ID = 'dc77c852-7635-4522-bc1c-e431c5d68b55'
+export const ATTRIBUTE_ANAC_IN_CONVALIDA_ID = '97dec753-8a6e-4a25-aa02-95ac8602b364'
+
 export const downloadCSVMock = (): Promise<string> => Promise.resolve(csvFileContent)
 
 export const internalAssignCertifiedAttributeMock = (_tenantOrigin: string, _tenantExternalId: string, _attributeOrigin: string, _attributeExternalId: string, _context: InteropContext): Promise<void> => Promise.resolve()
@@ -23,7 +28,20 @@ export const internalRevokeCertifiedAttributeMock = (_tenantOrigin: string, _ten
 export const getPATenantsMock = (ipaCodes: string[]): Promise<PersistentTenant[]> => Promise.resolve(ipaCodes.map(c => ({ ...persistentTenant, externalId: { origin: 'tenantOrigin', value: c } })))
 export const getNonPATenantsMock = (taxCodes: string[]): Promise<PersistentTenant[]> => Promise.resolve(taxCodes.map(c => ({ ...persistentTenant, externalId: { origin: 'tenantOrigin', value: c } })))
 export const getTenantByIdMock = (tenantId: string): Promise<PersistentTenant> => Promise.resolve({ ...persistentTenant, id: tenantId })
-export const getAttributeByExternalIdMock = (origin: string, code: string): Promise<PersistentAttribute> => Promise.resolve({ ...persistentAttribute, origin, code })
+export const getAttributeByExternalIdMock = (origin: string, code: string): Promise<PersistentAttribute> =>
+// Promise.resolve({ ...persistentAttribute, origin, code })
+{
+  switch (code) {
+    case ANAC_ABILITATO_CODE:
+      return Promise.resolve({ ...persistentAttribute, id: ATTRIBUTE_ANAC_ABILITATO_ID, origin, code })
+    case ANAC_IN_CONVALIDA_CODE:
+      return Promise.resolve({ ...persistentAttribute, id: ATTRIBUTE_ANAC_IN_CONVALIDA_ID, origin, code })
+    case ANAC_INCARICATO_CODE:
+      return Promise.resolve({ ...persistentAttribute, id: ATTRIBUTE_ANAC_INCARICATO_ID, origin, code })
+    default:
+      return Promise.reject(new Error('Unexpected attribute code'))
+  }
+}
 
 export const persistentTenant: PersistentTenant = {
   id: '091fbea1-0c8e-411b-988f-5098b6a33ba7',
@@ -36,4 +54,10 @@ export const persistentAttribute: PersistentAttribute = {
   id: '7a04c906-1525-4c68-8a5b-d740d77d9c80',
   origin: 'attributeOrigin',
   code: 'attributeCode'
+}
+
+export const persistentTenantAttribute: PersistentTenantAttribute = {
+  id: '7a04c906-1525-4c68-8a5b-d740d77d9c80',
+  type: 'PersistentCertifiedAttribute',
+  assignmentTimestamp: new Date()
 }
--- jobs/anac-certified-attributes-importer/src/service/__tests__/processor.test.ts
+++ jobs/anac-certified-attributes-importer/src/service/__tests__/processor.test.ts
@@ -1,6 +1,7 @@
 import { InteropTokenGenerator, ReadModelClient, RefreshableInteropToken, generateInternalTokenMock } from "@interop-be-reports/commons"
 import { ReadModelQueries, SftpClient, TenantProcessService, importAttributes } from "../index.js"
-import { downloadCSVMock, getAttributeByExternalIdMock, getNonPATenantsMock, getPATenantsMock, getTenantByIdMock, internalAssignCertifiedAttributeMock, internalRevokeCertifiedAttributeMock, sftpConfigTest } from "./helpers.js"
+import { ATTRIBUTE_ANAC_ABILITATO_ID, ATTRIBUTE_ANAC_IN_CONVALIDA_ID, downloadCSVMock, getAttributeByExternalIdMock, getNonPATenantsMock, getPATenantsMock, getTenantByIdMock, internalAssignCertifiedAttributeMock, internalRevokeCertifiedAttributeMock, persistentAttribute, persistentTenant, persistentTenantAttribute, sftpConfigTest } from "./helpers.js"
+import { PersistentTenant } from "../../model/tenant.model.js"
 
 
 describe('ANAC Certified Attributes Importer', () => {
@@ -18,7 +19,7 @@ describe('ANAC Certified Attributes Importer', () => {
 
   const refreshableInternalTokenSpy = vi.spyOn(refreshableTokenMock, 'get').mockImplementation(generateInternalTokenMock)
 
-  const downloadCSVSpy = vi.spyOn(sftpClientMock, 'downloadCSV').mockImplementation(downloadCSVMock)
+  // const downloadCSVSpy = vi.spyOn(sftpClientMock, 'downloadCSV').mockImplementation(downloadCSVMock)
 
   const internalAssignCertifiedAttributeSpy = vi.spyOn(tenantProcessMock, 'internalAssignCertifiedAttribute').mockImplementation(internalAssignCertifiedAttributeMock)
   const internalRevokeCertifiedAttributeSpy = vi.spyOn(tenantProcessMock, 'internalRevokeCertifiedAttribute').mockImplementation(internalRevokeCertifiedAttributeMock)
@@ -40,6 +41,8 @@ describe('ANAC Certified Attributes Importer', () => {
   })
 
   it('should succeed', async () => {
+    const downloadCSVSpy = vi.spyOn(sftpClientMock, 'downloadCSV').mockImplementation(downloadCSVMock)
+
     await run()
 
     expect(downloadCSVSpy).toBeCalledTimes(1)
@@ -55,15 +58,43 @@ describe('ANAC Certified Attributes Importer', () => {
 
   })
 
-  it('should succeed, assigning missing attributes', async () => { })
+  it('should succeed, assigning only missing attributes', async () => {
+    const csvFileContent =
+      `cf_gestore,denominazione,domicilio_digitale,codice_ipa,anac_incaricato,anac_abilitato,anac_in_convalida
+0123456789,Nome ente presente in IPA,gsp1@pec.it,DRMEST,TRUE,TRUE,TRUE`
 
-  it('should succeed, unassigning existing attributes', async () => { })
+    const localDownloadCSVMock = (): Promise<string> => Promise.resolve(csvFileContent)
+    const downloadCSVSpy = vi.spyOn(sftpClientMock, 'downloadCSV').mockImplementation(localDownloadCSVMock)
+
+    const readModelTenants: PersistentTenant[] = [{
+      ...persistentTenant,
+      externalId: { origin: 'IPA', value: 'DRMEST' },
+      attributes: [{ ...persistentTenantAttribute, id: ATTRIBUTE_ANAC_ABILITATO_ID }]
+    }]
+
+    const getPATenantsMock = (_ipaCodes: string[]): Promise<PersistentTenant[]> => Promise.resolve(readModelTenants)
+    const getPATenantsSpy = vi.spyOn(readModelQueriesMock, 'getPATenants').mockImplementation(getPATenantsMock)
+
+    await run()
+
+    expect(downloadCSVSpy).toBeCalledTimes(1)
+    expect(getTenantByIdSpy).toBeCalledTimes(1)
+    expect(getAttributeByExternalIdSpy).toBeCalledTimes(3)
+
+    expect(getPATenantsSpy).toBeCalledTimes(1)
+    expect(getNonPATenantsSpy).toBeCalledTimes(0)
+
+    expect(refreshableInternalTokenSpy).toBeCalledTimes(2)
+    expect(internalAssignCertifiedAttributeSpy).toBeCalledTimes(2)
+    expect(internalRevokeCertifiedAttributeSpy).toBeCalledTimes(0)
+
+  })
+
+  it('should succeed, unassigning only existing attributes', async () => { })
 
   it('should succeed, only for tenants that exist on read model ', async () => { })
 
   it('should succeed with more than one batch', async () => { })
-
-  it('should succeed without assigning and unassigning attributes if not required', async () => { })
 
   it('should fail on CSV retrieve error', async () => { })
 
