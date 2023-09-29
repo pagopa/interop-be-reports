ba5ec77133cf35971cfa65b24666ededb1e91247
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-09-29 11:53:03 +0200

    PIN-3813-3: Update anac file header (#31)

--- jobs/anac-certified-attributes-importer/src/model/csv-row.model.ts
+++ jobs/anac-certified-attributes-importer/src/model/csv-row.model.ts
@@ -1,25 +1,38 @@
 import { z } from 'zod'
 
-export const PaRow = z.object({
-  cf_gestore: z.string().min(1),
-  denominazione: z.string().min(1),
-  domicilio_digitale: z.string().min(1),
-  codice_ipa: z.string().min(1),
-  anac_incaricato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-  anac_abilitato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-  anac_in_convalida: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
+const Row = z.object({
+  codiceFiscaleGestore: z.string().min(1),
+  denominazioneGestore: z.string().min(1),
+  PEC: z.string().min(1),
+  ANAC_incaricato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
+  ANAC_abilitato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
+  ANAC_in_convalida: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
 })
-export type PaRow = z.infer<typeof PaRow>
 
-export const NonPaRow = z.object({
-  cf_gestore: z.string().min(1),
-  denominazione: z.string().min(1),
-  domicilio_digitale: z.string().min(1),
-  anac_incaricato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-  anac_abilitato: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-  anac_in_convalida: z.string().transform((value) => value.toUpperCase() === 'TRUE'),
-})
+export const NonPaRow = Row.transform(value => ({
+  cf_gestore: value.codiceFiscaleGestore,
+  denominazione: value.denominazioneGestore,
+  domicilio_digitale: value.PEC,
+  anac_incaricato: value.ANAC_incaricato,
+  anac_abilitato: value.ANAC_abilitato,
+  anac_in_convalida: value.ANAC_in_convalida,
+}))
+
 export type NonPaRow = z.infer<typeof NonPaRow>
+
+export const PaRow = Row.extend({
+  codiceIPA: z.string().min(1)
+}).transform(value => ({
+  cf_gestore: value.codiceFiscaleGestore,
+  denominazione: value.denominazioneGestore,
+  domicilio_digitale: value.PEC,
+  codice_ipa: value.codiceIPA,
+  anac_incaricato: value.ANAC_incaricato,
+  anac_abilitato: value.ANAC_abilitato,
+  anac_in_convalida: value.ANAC_in_convalida,
+}))
+
+export type PaRow = z.infer<typeof PaRow>
 
 export const CsvRow = PaRow.or(NonPaRow)
 
