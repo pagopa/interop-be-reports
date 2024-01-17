8d3074ea7aaad33c5fee4db6bd3da136e7060508
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-01-17 14:54:32 +0100

    Update helpers.utils.ts

--- jobs/dtd-metrics/src/utils/helpers.utils.ts
+++ jobs/dtd-metrics/src/utils/helpers.utils.ts
@@ -69,6 +69,8 @@ export function json2csv(data: object[]): string {
 }
 
 export function splitArrayIntoChunks<T>(array: T[], chunkSize: number): T[][] {
+  if (chunkSize <= 0) throw new Error(`Invalid chunk size: ${chunkSize}`)
+
   const chunks: T[][] = []
   for (let i = 0; i < array.length; i += chunkSize) {
     chunks.push(array.slice(i, i + chunkSize))
